5fa30107ed7e224c7cf92d78f50746066c88ba9a
Paul Boone <narcissus.pong@gmail.com>
2021-03-05 00:31:48 -0500

    functionalize all uio66 and uio67 linkers

--- functionalize_linkers.py
+++ functionalize_linkers.py
@@ -1,39 +1,39 @@
 from collections import Counter
 from pathlib import Path
 
-import numpy as np
-
 from lammps_tools.forcefields.uff.parameterize import get_pair_potential
+import numpy as np
 
 from mofun import Atoms, replace_pattern_in_structure
 from mofun.uff4mof import uff_key_starts_with
 
-structure_path = "mofs/uio-66.cif"
-linker_path = "linkers-cml/uio66.cml"
-fnlinker_path = "linkers-lmpdat/uio66-F.lmp-dat"
+def functionalize_structure_with_linkers(structure, linker, fnlinkerglob):
+    for fnlinker_path in fnlinkerglob:
+        print("reading %s" %fnlinker_path)
+        fnlinker = Atoms.from_lammps_data(open(fnlinker_path,"r"))
+        fnlinker.atom_type_labels = [str(i) for i in range(fnlinker.num_atom_types)]
+        try:
+            new_structure = replace_pattern_in_structure(structure, linker, fnlinker)
+            with open(lmp_base_path.joinpath(fnlinker_path.stem + ".lmp-dat"), "w") as fd:
+                new_structure.to_lammps_data(fd)
+        except Exception as e:
+            print("ERROR! ", e.args)
 
-structure = Atoms.from_cif(structure_path)
-linker = Atoms.from_cml(linker_path)
-fnlinker = Atoms.from_lammps_data(open(fnlinker_path,"r"))
+def assign_pair_params_to_structure(structure):
+    # NOTE: in UFF, pair params should always be the same for atoms of the same element, regardless of type
+    pair_uff_keys = [uff_key_starts_with(el.ljust(2, "_"))[0] for el in structure.atom_type_elements]
+    structure.atom_type_labels = pair_uff_keys
+    pair_params = get_pair_potential(pair_uff_keys)
+    structure.pair_params = ['%10.6f %10.6f # %s' % (*pair_params[label], label) for label in pair_uff_keys]
 
-# NOTE: in UFF, pair params should always be the same for atoms of the same element, regardless of type
-pair_uff_keys = [uff_key_starts_with(el.ljust(2, "_"))[0] for el in structure.atom_type_elements]
-pair_params = get_pair_potential(pair_uff_keys)
-structure.pair_params = ['%10.6f %10.6f # %s' % (*pair_params[label], label) for label in pair_uff_keys]
+lmp_base_path = Path("mofs-functionalized")
 
-# structure.angles
+linker = Atoms.from_cml(Path("linkers-cml/uio66.cml"))
+structure = Atoms.from_cif("mofs/uio-66.cif")
+assign_pair_params_to_structure(structure)
+functionalize_structure_with_linkers(structure, linker, Path("linkers-lmpdat").glob("uio66-*"))
 
-new_structure = replace_pattern_in_structure(structure, linker, fnlinker)
-
-
-#checks
-Counter(structure.elements)
-Counter(new_structure.elements)
-
-set(new_structure.elements)
-fnlinker.atom_types
-fnlinker.pair_params
-structure.atom_types
-new_structure.pair_params
-new_structure.bond_type_params
-new_structure.angle_type_params
+linker = Atoms.from_cml(Path("linkers-cml/uio67.cml"))
+structure = Atoms.from_cif("mofs/uio-67.cif")
+assign_pair_params_to_structure(structure)
+functionalize_structure_with_linkers(structure, linker, Path("linkers-lmpdat").glob("uio67-*"))
