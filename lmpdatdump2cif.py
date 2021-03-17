a69268165c7c18080cd245b50292c3197786ac42
Paul Boone <narcissus.pong@gmail.com>
2021-03-17 19:46:32 -0400

    add check for overlapped atoms

--- lmpdatdump2cif.py
+++ lmpdatdump2cif.py
@@ -7,6 +7,7 @@ output a cif.
 import ase.io
 import click
 import numpy as np
+from scipy.spatial import distance
 
 from mofun import Atoms
 
@@ -15,12 +16,19 @@ from mofun import Atoms
 @click.option('--dumppath', type=click.Path())
 @click.option('--outpath', '-o', type=click.Path())
 def lmpdat2cif(lmpdatpath, dumppath=None, outpath=None):
-    atoms = Atoms.from_lammps_data(open(lmpdatpath, "r"))
+    atoms = Atoms.from_lammps_data(open(lmpdatpath, "r"), use_comment_for_type_labels=True)
     if dumppath is not None:
         # update positions in original atoms file with new positions
         dumpatoms = ase.io.read(dumppath, format="lammps-dump-text")
         assert len(dumpatoms.positions) == len(atoms.positions)
         atoms.positions = dumpatoms.positions
+        atoms.to_lammps_data(open("test.lmpdat", "w"))
+
+    # Overlapped atom position check
+    s_ss = distance.cdist(atoms.positions, atoms.positions, "sqeuclidean")
+    zero_indices = [(i1,i2) for i1, i2 in np.argwhere(s_ss < 0.1) if i1 < i2]
+    for i1, i2 in zero_indices:
+        print("WARNING: some atoms look like they are overlapped: %d %d dist=%6.4f" % (i1+1, i2+1, s_ss[i1,i2] ** 0.5))
 
     dumpatoms.set_pbc(True)
     bonddists = np.array([dumpatoms.get_distance(b1,b2, mic=True)for b1, b2 in atoms.bonds])
