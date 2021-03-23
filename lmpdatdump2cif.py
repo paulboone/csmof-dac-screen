08de6dd0a784c15d9a596f60b0e7bfae0679fae8
Paul Boone <narcissus.pong@gmail.com>
2021-03-22 21:47:54 -0400

    can no specify an element to convert all group 0 framework atoms to so any functionalized linkers are easier to see in the CIF

--- lmpdatdump2cif.py
+++ lmpdatdump2cif.py
@@ -15,16 +15,19 @@ from mofun import Atoms
 @click.argument('lmpdatpath', type=click.Path())
 @click.option('--dumppath', type=click.Path())
 @click.option('--outpath', '-o', type=click.Path())
-def lmpdat2cif(lmpdatpath, dumppath=None, outpath=None):
+@click.option('--framework-element', type=str, help="convert all atoms that are in group 0, the framework group to a specific atom type to make vizualizing the structure easier")
+def lmpdat2cif(lmpdatpath, dumppath=None, outpath=None, framework_element=None):
     atoms = Atoms.from_lammps_data(open(lmpdatpath, "r"), use_comment_for_type_labels=True)
     if dumppath is not None:
         # update positions in original atoms file with new positions
         dumpatoms = ase.io.read(dumppath, format="lammps-dump-text")
         assert len(dumpatoms.positions) == len(atoms.positions)
         atoms.positions = dumpatoms.positions
-        atoms.to_lammps_data(open("test.lmpdat", "w"))
 
     aseatoms = atoms.to_ase()
+    if framework_element is not None:
+        aseatoms.symbols[atoms.atom_groups == 0] = framework_element
+
     aseatoms.set_pbc(True)
     if outpath is None:
         aseatoms.write('-', format="cif")
