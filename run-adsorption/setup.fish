2f4d85a190fdc3af7fcba12bbc1238367a2d9b3a
Paul Boone <narcissus.pong@gmail.com>
2021-04-15 10:13:59 -0400

    use 1 1 1 unit cells for uio67; fix pathing

--- run-adsorption/setup.fish
+++ run-adsorption/setup.fish
@@ -1,4 +1,6 @@
-for mof in ../mofs-functionalized-mols/*.mol
+
+
+for mof in ../run-relax-NVT/mofs-relaxed-mols/*.mol
   set mofname (basename $mof .mol)
   for raspa_input in *.input
     set gasprocess (basename $raspa_input .input)
@@ -9,5 +11,8 @@ for mof in ../mofs-functionalized-mols/*.mol
     cp $raspa_input $mofprocessdir
 
     gsed -i "s/FrameworkName.*/FrameworkName $mofname/g" $mofprocessdir/$gasprocess.input
+    if string match "uio67*" $mofname
+      gsed -i "s/UnitCells.*/UnitCells 1 1 1/g" $mofprocessdir/$gasprocess.input
+    end
   end
 end
