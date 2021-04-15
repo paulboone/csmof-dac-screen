d339e8d61cead811c761eb6ae9da2dddc03f7303
Paul Boone <narcissus.pong@gmail.com>
2021-04-15 10:14:37 -0400

    fix pathing

--- run-relax-NVT/run.fish
+++ run-relax-NVT/run.fish
@@ -16,8 +16,8 @@ for cif in mofs-relaxed-cifs/*.cif
     set mof (basename $cif .cif)
     echo $mof
     obabel -i cif $cif --partialcharge eqeq -o mol2 -O /dev/null --print > charges/$mof.charges
-    python3 ../cif2raspamol_wcharges.py $cif charges/$mof.charges mofs-relaxed-mols/$mof.mol
-    python3 ../cif2lmpdat_wcharges.py $cif charges/$mof.charges mofs-relaxed-lmpdat/$mof.lmpdat
+    python3 ./cif2raspamol_wcharges.py $cif charges/$mof.charges mofs-relaxed-mols/$mof.mol
+    python3 ./cif2lmpdat_wcharges.py $cif charges/$mof.charges mofs-relaxed-lmpdat/$mof.lmpdat
 end
 
 grep "Dangerous builds" */log.lammps > dangerous-builds.out
