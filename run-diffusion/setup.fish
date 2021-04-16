7ef5b53a6a0283b941927d31c3f174636ceb44fc
Paul Boone <narcissus.pong@gmail.com>
2021-04-16 11:45:15 -0400

    enforce minimum image convention

--- run-diffusion/setup.fish
+++ run-diffusion/setup.fish
@@ -1,16 +1,17 @@
 
 # make independent configs
-for mof in ../run-relax-NVT/mofs-relaxed-lmpdat/*CF3-2.lmpdat
-  set mofname (basename $mof .lmpdat)
+for mof in ../run-relax-NVT/mofs-relaxed-cifs/*CF3-2.cif
+  set mofname (basename $mof .cif)
 
-  for gas in *.lmpdat
-    set gasname (basename $gas .lmpdat)
+  for gas in *.lammps
+    set gasname (basename $gas .lammps)
     set mofgasdir $mofname-$gasname
     mkdir $mofgasdir
-    cp $mof $mofgasdir/
-    obabel -i cif ../run-relax-NVT/mofs-relaxed-cifs/$mofname.cif -o xyz -O $mofgasdir/$mofname.xyz
+
     cd $mofgasdir/
-    python3 ../packmol_gaslmpdat.py -n 100 $mofname.xyz ../$gasname.lmpdat ../$gasname.xyz
+    python3 ../../run-relax-NVT/cif2lmpdat.py --mic 12.8 ../$mof $mofname.lmpdat
+    python3 ../../lmpdatdump2cif.py $mofname.lmpdat -o $mofname.xyz
+    python3 ../packmol_gaslmpdat.py -n 10 {$mofname}.lmpdat {$mofname}.xyz ../$gasname.lmpdat ../$gasname.xyz
     cd ..
   end
 end
