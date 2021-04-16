522902c985501f47f0693660d2c25fb45ff994fc
Paul Boone <narcissus.pong@gmail.com>
2021-04-16 16:56:38 -0400

    make changes to lammps config per mof

--- run-diffusion/setup.fish
+++ run-diffusion/setup.fish
@@ -12,6 +12,14 @@ for mof in ../run-relax-NVT/mofs-relaxed-cifs/*CF3-2.cif
     python3 ../../run-relax-NVT/cif2lmpdat.py --mic 12.8 ../$mof $mofname.lmpdat
     python3 ../../lmpdatdump2cif.py $mofname.lmpdat -o $mofname.xyz
     python3 ../packmol_gaslmpdat.py -n 10 {$mofname}.lmpdat {$mofname}.xyz ../$gasname.lmpdat ../$gasname.xyz
+    cp ../$gas ./
+
+    set randomseed (random)
+    gsed -i "s/variable randomSeed equal.*/variable randomSeed equal $randomseed/g" $gas
+
+    set numatomtypes (grep ".* atom types" $mofname.lmpdat | cut -f 1 -d ' ')
+    gsed -i "s/variable mofAtomTypes equal.*/variable mofAtomTypes equal $numatomtypes/g" $gas
+
     cd ..
   end
 end
