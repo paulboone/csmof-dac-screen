052fb7af0b092e0fbda1a24deebcbf7538dfd424
Paul Boone <narcissus.pong@gmail.com>
2021-04-27 08:47:44 -0400

    echo status of files as creating them; refactor so blocks not repeated for each gas

--- run-diffusion/process_data.fish
+++ run-diffusion/process_data.fish
@@ -1,6 +1,31 @@
-for sim in uio*co2
-  cd $sim
-  lammpstrj_to_npy.py ./gas.lammpstrj -f gastrj.npy --atoms-per-molecule 3 --start-molecule-index 2
-  npytraj_diffusivity.py ./gastrj.npy --average_rows 4000 --output-molecule-plots
-  cd ..
+
+function _converttrj
+  set atoms_per_molecule $argv[1]
+  set mofs $argv[2..-1]
+
+  echo $atoms_per_molecule
+  echo $mofs
+  for sim in $mofs
+    if not test -f $sim/gastrj.npy
+      cd $sim
+      echo $sim
+      if test -f ./output/gas.lammpstrj.gz
+        lammpstrj_to_npy.py ./output/gas.lammpstrj.gz -f gastrj.npy --atoms-per-molecule $atoms_per_molecule --start-molecule-index 2
+      else
+        echo $sim: NO ./output/gas.lammpstrj.gz
+      end
+      if test -f ./gastrj.npy
+        npytraj_diffusivity.py ./gastrj.npy --average-rows 1 --fs-per-row 1000 --output-molecule-plots > diffusivities.out
+      else
+        echo $sim: NO ./gastrj.npy
+      end
+      cd ..
+    end
+  end
 end
+
+_converttrj 3 uio*-co2
+_converttrj 3 uio*-n2
+_converttrj 4 uio*-tip4p
+
+functions -e _converttrj
