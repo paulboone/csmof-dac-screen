3cfc534e3979d842684f3238ee7116d3ddd5dfd6
Paul Boone <narcissus.pong@gmail.com>
2021-05-18 11:37:14 -0400

    process data script (ROUGH)

---
+++ run-adsorption/process_data.fish
@@ -0,0 +1,11 @@
+
+
+
+
+python3 extract_henrys.py
+
+python3 ../../csmof-dac-screening/run-adsorption/extract_loadings.py "uio*_stp/results/Output/System_0/*.data" > loadings_stp.csv
+python3 ../../csmof-dac-screening/run-adsorption/extract_loadings.py "uio*_desorb/results/Output/System_0/*.data" > loadings_desorb.csv
+
+python3 ../../csmof-dac-screening/run-adsorption/extract_henrys.py "uio*_henrys/results/Output/System_0/*.data" > henrys_stp.csv
+python3 ../../csmof-dac-screening/run-adsorption/extract_henrys.py "uio*_henrys2/results/Output/System_0/*.data" > henrys_desorb.csv
