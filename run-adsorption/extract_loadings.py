1bbd8d77062bc2841a5efdc2fce5bfda6feaa33a
Paul Boone <narcissus.pong@gmail.com>
2021-05-18 11:35:50 -0400

    convert to generalized click script

--- run-adsorption/extract_loadings.py
+++ run-adsorption/extract_loadings.py
@@ -3,23 +3,23 @@ import csv
 from pathlib import Path
 import sys
 
+import click
+
 from mofun.raspa_output import parse_gas_loading
 
+@click.command()
+@click.argument('glob')
+def extract_loadings(glob):
+    loadingcsv = csv.writer(sys.stdout)
+    loadingcsv.writerow(["mof", "gas", "pressure", "loading", "loading err",
+                        "block1", "block2", "block3", "block4", "block5", "conv"])
 
-loadingcsv = csv.writer(sys.stdout)
-loadingcsv.writerow(["mof", "gas", "pressure", "loading", "loading err",
-                    "block1", "block2", "block3", "block4", "block5", "conv"])
+    for outputpath in Path("./").glob(glob):
+        simname = outputpath.parts[0]
+        mof, gas, pressure = simname.split("_")
+        alldata = parse_gas_loading(outputpath)
+        loading, loading_error = alldata[0:2]
+        loadingcsv.writerow([mof, gas, pressure, *alldata])
 
-for outputpath in Path("../2021-04-15-ads/").glob("uio*_stp/results/Output/System_0/*.data"):
-    simname = outputpath.parts[2]
-    mof, gas, pressure = simname.split("_")
-    alldata = parse_gas_loading(outputpath)
-    loading, loading_error = alldata[0:2]
-    loadingcsv.writerow([mof, gas, pressure, *alldata])
-
-for outputpath in Path("../2021-04-15-ads/").glob("uio*_desorb/results/Output/System_0/*.data"):
-    simname = outputpath.parts[2]
-    mof, gas, pressure = simname.split("_")
-    alldata = parse_gas_loading(outputpath)
-    loading, loading_error = alldata[0:2]
-    loadingcsv.writerow([mof, gas, pressure, *alldata])
+if __name__ == '__main__':
+    extract_loadings()
