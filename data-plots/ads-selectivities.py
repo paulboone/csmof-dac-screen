0636c3dc167f97d7892ae96729e862f5f3668f92
Paul Boone <narcissus.pong@gmail.com>
2021-07-01 15:11:03 -0400

    updated plots for adsorption selectivity

--- data-plots/ads-selectivities.py
+++ data-plots/ads-selectivities.py
@@ -5,106 +5,103 @@ from matplotlib import rc
 import numpy as np
 import pandas as pd
 
-
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-# rc('text', usetex=True)
 
-fsl = fs = 8
+fsl = fs = 9
 
 @click.command()
 @click.argument('csv-path', type=click.File())
 @click.option('--outputpath', '-o', type=click.Path(), default="ads-selectivity.png")
-def ads_selectivity(csv_path, outputpath="ads-selectivity.png"):
+@click.option('--plotnum', '-n', type=int, default=0)
+def ads_selectivity(csv_path, outputpath="ads-selectivity.png", plotnum=0):
+    fig = plt.figure(figsize=(6, 6))
 
-    fig = plt.figure(figsize=(8,8))
-
-    # cm = matplotlib.cm.get_cmap("viridis")
-    data = pd.read_csv(csv_path)
-    data["log_CO2_H2O"] = np.log10(data["CO2/H2O"])
-    data["log_CO2_N2"] = np.log10(data["CO2/N2"])
-    data["sizes"] = np.where(data.CO2 < 5e-4, 2,5)
-    uio66 = data[data.mof.str.startswith("uio66")]
-    uio67 = data[data.mof.str.startswith("uio67")]
-    # adsdata
-
-    # sizes = np.log10(data["CO2"] + 0.000001) + 7
+    data = pd.read_csv(csv_path) # index_col=["mof"]
+    data["log_CO2_H2O"] = np.log10(data["co2/h2o selectivity"])
+    data["log_CO2_N2"] = np.log10(data["co2/n2 selectivity"])
+    data["moflabels"] = list(data.mof.str.replace("UIO-6[67] ", ""))
+    uio66 = data[data.mof.str.startswith("UIO-66")]
+    uio67 = data[data.mof.str.startswith("UIO-67")]
 
 
-
-
-    # def marker_label(mof):
-    #     if mof.startswith("uio66"):
-    #         marker='o'
-    #     else:
-    #         marker='s'
-    #     label = "-".join(mof.split("-")[1:])
-    #     if label == "":
-    #         label = mof.split("-")[0]
-    #     return marker, label
-
-    # markers, labels = list(zip(*[marker_label(mof) for mof in data['mof']]))
     ax = fig.subplots(ncols=1)
+    ax.grid(which='major', axis='both', linestyle='-', color='0.9', zorder=10)
+    ax.set_xlabel('log$_{10}$ CO$_2$/H$_2$O adsorption selectivity', fontsize=fsl)
+    ax.set_ylabel('log$_{10}$ CO$_2$/N$_2$ adsorption selectivity', fontsize=fsl)
 
-    # ax.set_xscale('log')
-    # ax.set_yscale('log')
-    # print(np.min(data["CO2/H2O"]), np.max(data["CO2/H2O"]))
-    # print(np.min(data["CO2/N2"]), np.max(data["CO2/N2"]))
-    # ax.set_xlim(-3, 3)
-    # ax.set_ylim(-1, 3)
+    def anno(idx, alignment='left', xytextoffset=(0,0), fontsize=7):
+        x, y = (data.log_CO2_H2O[idx], data.log_CO2_N2[idx])
+        if alignment=='left':
+            xytext = (4, -0.5)
+        else:
+            xytext = (-4, -0.5)
+        ax.annotate(data.moflabels[idx] + " - %3.2f" % (float(data.loading_co2[idx])), (x, y), size=5,
+            xytext=(xytext[0] + xytextoffset[0], xytext[1] + xytextoffset[1]), textcoords='offset points', horizontalalignment=alignment, verticalalignment='center', fontsize=fontsize)
 
+    if plotnum == 0:
+        sc1 = ax.scatter(uio66.log_CO2_H2O, uio66.log_CO2_N2, zorder=2, s=10, marker=">", label="UIO-66", c="sienna")
+        sc2 = ax.scatter(uio67.log_CO2_H2O, uio67.log_CO2_N2, zorder=2, s=20, marker= r'$\bowtie$', label="UIO-67", c="mediumseagreen")
+        ax.set_xlim(-3, 3)
+        ax.set_xticks([-2., -1, 0., 1, 2])
+        ax.set_ylim(-1, 3)
+        ax.set_yticks([0., 1., 2.])
 
-    # adsorption zoom in
-    # ax.set_xlim(1, 2.75)
-    # ax.set_ylim(0.75, 2.75)
+    elif plotnum == 1:
+        sc1 = ax.scatter(uio66.log_CO2_H2O, uio66.log_CO2_N2, zorder=2, s=10, marker=">", label="UIO-66", c="sienna")
+        sc2 = ax.scatter(uio67.log_CO2_H2O, uio67.log_CO2_N2, zorder=2, s=20, marker= r'$\bowtie$', label="UIO-67", c="mediumseagreen")
+        ax.set_xlim(0.5, 3.)
+        ax.set_xticks([1., 2.])
+        ax.set_ylim(0.5, 3.)
+        ax.set_yticks([1., 2.])
 
+        subplot=2
+        for idx in data.index:
+            mof = data.mof[idx]
+            x, y = (data.log_CO2_H2O[idx], data.log_CO2_N2[idx])
+            if subplot==0:
+                if mof in ["UIO-66 branched-HNC$_5$", "UIO-66 2x ring-HNC$_5$"]:
+                    anno(idx, 'left')
+                elif mof in ["UIO-66 2x NC$_4$", "UIO-66 2x alkane-OC$_4$"]:
+                    anno(idx, 'right')
+            elif subplot==1:
+                if (0.5 < x < 3.) and (0.5 < y < 3.):
+                    anno(idx, "left", fontsize=5)
+            elif subplot==2:
+                if mof in ["UIO-67 2x NC$_4$", "UIO-67 branched-HNC$_5$"]:
+                    anno(idx, 'right')
+                elif mof in [ "UIO-66", "UIO-67",
+                            "UIO-66 2x NC$_4$",
+                            "UIO-66 branched-HNC$_5$",
+                            "UIO-66 2x N$_3$", "UIO-67 2x N$_3$",
+                            "UIO-66 2x NH$_2$", "UIO-67 2x NH$_2$"]:
+                    anno(idx, "left")
+    elif plotnum == 2:
+        sc2 = ax.scatter(uio67.log_CO2_H2O, uio67.log_CO2_N2, zorder=2, s=20, marker= r'$\bowtie$', label="UIO-67", c="mediumseagreen")
+        ax.set_xlim(0.75, 1.6)
+        ax.set_xticks([1., 1.5,])
+        ax.set_ylim(0.75, 1.6)
+        ax.set_yticks([1., 1.5])
+        uio67_inplot = uio67[uio67.log_CO2_H2O.between(0.75, 1.6) & uio67.log_CO2_N2.between(0.75, 1.6)]
 
-    sc1 = ax.scatter(uio66.log_CO2_H2O, uio66.log_CO2_N2, zorder=2, marker="v", label="uio-66", c="sienna", s=12)
-    sc2 = ax.scatter(uio67.log_CO2_H2O, uio67.log_CO2_N2, zorder=2, marker= r'$\bowtie$', label="uio-67", c="mediumseagreen", s=12)
-    #
-    # sc1 = ax.scatter(uio66["log_CO2_H2O"], uio66["log_CO2_N2"], s=uio66.sizes, zorder=2, marker="o", label="uio-66")
-    # sc2 = ax.scatter(uio67["log_CO2_H2O"], uio67["log_CO2_N2"], s=uio67.sizes, zorder=2, marker="s", label="uio-67")
-
-    # # ax.set_xticks(prop1range[1] * np.array([0.0, 0.25, 0.5, 0.75, 1.0]))
-    # ax.set_yticks(prop2range[1] * np.array([0.0, 0.25, 0.5, 0.75, 1.0]))
-    # ax.set_xticks(prop1range[1] * np.array(range(0,num_bins + 1))/num_bins, minor=True)
-    # ax.set_yticks(prop2range[1] * np.array(range(0,num_bins + 1))/num_bins, minor=True)
-    #
-    # ax.tick_params(axis='x', which='major', labelsize=fs)
-    # ax.tick_params(axis='y', which='major', labelsize=fs)
-    #
-    # ax.grid(which='major', axis='both', linestyle='-', color='0.9', zorder=0)
-    # # ax.grid(which='minor', axis='both', linestyle='-', color='0.9', zorder=0)
-
-
-    # for i, (_, label) in enumerate(labels):
-    #     print(label, (data["CO2/H2O"][i], data["CO2/N2"][i]))
-    #     ax.annotate(label, (data["CO2/H2O"][i], data["CO2/N2"][i]), size=6)
-
-
-    for i, mof in enumerate(data.mof):
-        print(mof, (data["log_CO2_H2O"][i], data["log_CO2_N2"][i]))
-        # if data["log_CO2_H2O"][i] > 1 and data["log_CO2_N2"][i] > 0.5:
-        x, y = (data["log_CO2_H2O"][i], data["log_CO2_N2"][i])
-
-        # # desorption
-        # # if not ((0.5 < x < 2) and (0.8 < y < 2.)):
-        # ax.annotate(mof, (x, y), size=5, xytext=(x+0.02,y-0.02))
-
-        # adsorption
-        if not ((0.5 < x < 2) and (0.8 < y < 2.)):
-            ax.annotate(mof, (x, y), size=5, xytext=(x+0.03,y-0.03))
-
-        # # adsorption zoom
-        # if (1 < x < 2.75) and (0.75 < y < 2.75):
-        #     ax.annotate(mof, (x, y), size=5, xytext=(x+0.01,y-0.01))
-
-    ax.set_xlabel('log10 CO2/H2O ads selectivity', fontsize=fsl)
-    ax.set_ylabel('log10 CO2/N2 ads selectivity', fontsize=fsl)
+        for idx in uio67_inplot.index:
+            mof = uio67_inplot.mof[idx]
+            if mof == 'UIO-67 alkane-HNC$_3$':
+                anno(idx, "left", (0,-2))
+            elif mof == 'UIO-67 alkane-OC$_4$':
+                anno(idx, "right", (0,-3))
+            elif mof == 'UIO-67 NH$_2$':
+                anno(idx, "left", (0,-1))
+            elif mof == 'UIO-67 OH':
+                anno(idx, "center", (4,6))
+            elif mof in ['UIO-67 2x alkane-HNC$_3$', 'UIO-67 alkane-HNC$_5$', 'UIO-67 ring-HNC$_5$', 'UIO-67 N$_3$', 'UIO-67 alkane-HNC$_4$']:
+                anno(idx)
+            else:
+                anno(idx, "right")
 
     ax.legend()
-    # fig.subplots_adjust(wspace=0.05, hspace=0.05)
-    fig.savefig(outputpath, dpi=300, bbox_inches='tight')
+    fig.savefig(outputpath, dpi=300, bbox_inches='tight', transparent=True)
     plt.close(fig)
+
 
 if __name__ == '__main__':
     ads_selectivity()
