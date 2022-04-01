c6499b7edf3b7ad679a674837c5cdbefe7df4639
Paul Boone <narcissus.pong@gmail.com>
2022-04-01 14:42:57 -0400

    scores as ratios of UIO-67 base score

--- analysis/plot_scores.py
+++ analysis/plot_scores.py
@@ -1,38 +1,50 @@
+import math
+
+import click
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+@click.command()
+@click.argument('inputpath', type=click.Path())
+@click.argument('outputpath', type=click.Path())
+@click.option('--metric', '-m', type=str, default="cs_score_2")
+def plot_scores(inputpath, outputpath, metric="cs_score_2"):
+    score_name = {'cs_score': 'CO2 output stream ppm / 400 ppm',
+                  'cs_score_2': '[CO2 output stream ppm / 400 ppm - 1] x CO2 adsorbed [mol / cm3]',
+                  'cs_score_3': '[CO2 output stream ppm / 400 ppm - 1] x CO2 adsorbed / [same calc for UIO-67]',
+                  'cs_score_4': 'score 4'}
 
-score_metric = "cs_score_2"
-score_name = {'cs_score': 'fraction CO2 output stream / 400ppm',
-              'cs_score_2': '(fraction CO2 output stream / 400ppm) x CO2 adsorbed',}
-cs = pd.read_csv("scores-out.csv", usecols=["mof_shell", "mof_core", score_metric])
+    cs = pd.read_csv(inputpath, usecols=["mof_shell", "mof_core", metric])
+    grid = cs.pivot(index="mof_shell", columns="mof_core", values=metric)
+    all_mofnames = grid.columns
 
+    fig = plt.figure(figsize=(8,8), constrained_layout=True)
+    ax = fig.subplots()
+    grid_np = grid.to_numpy()
+    im = ax.imshow(grid_np, origin='lower')
+    cbar = ax.figure.colorbar(im, ax=ax)
 
-grid = cs.pivot(index="mof_shell", columns="mof_core", values=score_metric)
-print(grid.to_numpy())
-print(grid)
-all_mofnames = grid.columns
+    ax.set_title(score_name[metric])
+    ax.set_xticks(range(0,len(all_mofnames)))
+    ax.set_xticklabels(all_mofnames, rotation='vertical', fontsize=9)
+    ax.set_yticks(range(0,len(all_mofnames))) #, minor=True
+    ax.set_yticklabels(all_mofnames, fontsize=9)
+    ax.set_ylabel("SHELL")
+    ax.set_xlabel("CORE")
 
-fig = plt.figure(figsize=(8,8), constrained_layout=True)
-ax = fig.subplots()
-grid_np = grid.to_numpy()
-im = ax.imshow(grid_np, origin='lower')
-cbar = ax.figure.colorbar(im, ax=ax)
+    # label_threshold =
+    # print(np.flip(np.argsort(grid_np, axis=None)))
+    # print(np.ravel(grid_np).shape)
+    # print(np.ravel(grid_np)[34])
+    label_threshold = np.nanmean(grid_np) + 2 * np.nanstd(grid_np)
+    label_multiplier = 10**math.ceil(math.log10(1 / label_threshold) + 1)
+    print(label_threshold, label_multiplier)
+    for (y,x), v in np.ndenumerate(grid_np):
+        if v > label_threshold:
+            text = ax.text(x, y, "%2d" % (label_multiplier * v), ha="center", va="center", color="black")
 
-ax.set_title(score_name[score_metric])
-ax.set_xticks(range(0,len(all_mofnames)))
-ax.set_xticklabels(all_mofnames, rotation='vertical', fontsize=9)
-ax.set_yticks(range(0,len(all_mofnames))) #, minor=True
-ax.set_yticklabels(all_mofnames, fontsize=9)
-ax.set_ylabel("SHELL")
-ax.set_xlabel("CORE")
+    fig.savefig(outputpath, dpi=144)
 
-for (y,x), v in np.ndenumerate(grid_np):
-    if 1000 * v > 2.:
-        text = ax.text(x, y, "%2.1f" % (1000 * v), ha="center", va="center", color="black")
-
-# cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
-
-fig.savefig("size-normalized-%s.png" % score_metric, dpi=144)
-
+if __name__ == '__main__':
+    plot_scores()
--- analysis/scores.py
+++ analysis/scores.py
@@ -45,26 +45,27 @@ def find_breakthrough_time(x, *conc_args):
     print('<--bt')
     return co2_bt.root
 
-def calc_delta_breakthrough_time_for_shell_size(x_cs, co2_ads, co2_diff, h2o_ads, h2o_diff, threshold, delta_t):
-    v =  find_breakthrough_time(x_cs, h2o_ads, h2o_diff, threshold) - \
-           find_breakthrough_time(x_cs, co2_ads, co2_diff, threshold) - delta_t
-    print(" v = ", v)
+def calc_h2o_breakthrough_time_for_shell_size(x_cs,h2o_ads, h2o_diff, threshold, delta_t):
+    v =  find_breakthrough_time(x_cs, h2o_ads, h2o_diff, threshold) - delta_t
+    print(" bt_time - %s= " % delta_t, v)
     return v
 
 
-def find_shell_size_for_delta_breakthrough_time(*args):
+def find_shell_size_for_breakthrough_time(*args):
     print("----")
     print("find_shell_size: ", *args)
     try:
-        return root_scalar(calc_delta_breakthrough_time_for_shell_size, args, bracket=(1e-5, 0.3)).root
+        return root_scalar(calc_h2o_breakthrough_time_for_shell_size, args, bracket=(1e-5, 0.3)).root
     except ValueError:
         print("error")
         return 0.0
 
 def find_total_adsorption(t, ads, diff, shell_size):
     x_all = np.linspace(0., shell_size, 200)
+    print("find_total_adsorption, x_all: ", x_all)
     h2o_conc = np.array([conc(t, x, ads, diff, 0, 2*shell_size) for x in x_all])
-    return np.trapz(h2o_conc)
+    print("find_total_adsorption, h2o_conc per x: ", h2o_conc)
+    return np.trapz(h2o_conc, x_all)
 
 def add_plots_for_mof(axes, mof, x_all, t, ads_co2, diff_co2, ads_h2o, diff_h2o, x_csdiv):
     print(axes)
@@ -112,21 +113,22 @@ mofs.drop(labels=['d_co2_a2_fs', 'd_n2_a2_fs', 'd_h2o_a2_fs'], axis='columns', i
 # calculate breakthrough times
 # define the breakthrough limit to be 1% of the equilibrium loading of co2
 mofs['co2_breakthrough_limit'] = mofs['a_co2_mole_cm3'] / 100
-# mofs['shell_size_cm'] = shell_size
+
+# define the breakthrough limit to be 1% of the equilibrium loading of co2 in UIO-67
+# mofs['co2_breakthrough_limit'] = mofs[mofs.mof == "UIO-67"]['a_co2_mole_cm3'].values[0] / 100
+
+
+
+
+print(mofs.mof)
+# mofs = mofs[mofs.mof=="UIO-67"]
 
 # find shell size s.t ∆t = 100s
-
-
-# uio67 = mofs[mofs.mof=="UIO-67"]
-# print(uio67)
-# print("UIO-67")
-# print("shell size cm = ", [find_shell_size_for_delta_breakthrough_time(*args, 100) for args in
-#     uio67[['a_co2_mole_cm3', 'd_co2_cm2_s', 'a_h2o_mole_cm3', 'd_h2o_cm2_s', 'co2_breakthrough_limit']].itertuples(index=False)])
+process_time = 100 # seconds
+mofs['shell_size_cm'] = [find_shell_size_for_breakthrough_time(*args, process_time) for args in
+    mofs[['a_h2o_mole_cm3', 'd_h2o_cm2_s', 'co2_breakthrough_limit']].itertuples(index=False)]
 
 # assert 1 == 2
-mofs['shell_size_cm'] = [find_shell_size_for_delta_breakthrough_time(*args, 100) for args in
-    mofs[['a_co2_mole_cm3', 'd_co2_cm2_s', 'a_h2o_mole_cm3', 'd_h2o_cm2_s', 'co2_breakthrough_limit']].itertuples(index=False)]
-
 # (co2_ads, co2_diff, h2o_ads, h2o_diff, delta_t)
 
 mofs['co2_breakthrough_time_s'] = [find_breakthrough_time(*conc_args) for conc_args in
@@ -141,22 +143,39 @@ mofs['h2o_breakthrough_adsorption_mol_cm2'] = [find_total_adsorption(*conc_args)
     mofs[['h2o_breakthrough_time_s', 'a_h2o_mole_cm3', 'd_h2o_cm2_s', 'shell_size_cm']].itertuples(index=False)]
 
 
+
+
+
 shells = mofs
 cores = mofs[['mof', 'a_co2_mole_cm3', 'a_n2_mole_cm3']]
 
 cs = shells.join(cores, how="cross", lsuffix="_shell", rsuffix="_core")
-cs['core_size_cm'] = cs['co2_flux_mole_cm2s'] * cs['delta_t_s'] / cs['a_co2_mole_cm3_core']
-cs['cs_co2_mole_cm2'] = cs['a_co2_mole_cm3_shell'] * cs['shell_size_cm'] + cs['a_co2_mole_cm3_core'] * cs['core_size_cm']
-cs['cs_h2o_mole_cm2'] = cs['a_n2_mole_cm3_shell'] * cs['shell_size_cm']
-cs['cs_n2_mole_cm2'] = cs['a_n2_mole_cm3_shell'] * cs['shell_size_cm'] + cs['a_n2_mole_cm3_core'] * cs['core_size_cm']
-cs['cs_co2_fraction'] = cs['cs_co2_mole_cm2'] / (cs['cs_co2_mole_cm2'] + cs['cs_h2o_mole_cm2'] + cs['cs_n2_mole_cm2'])
+
+
+cs['core_radius_cm'] = 3 * cs['co2_flux_mole_cm2s'] * cs['delta_t_s'] / cs['a_co2_mole_cm3_core']
+cs['core_volume_cm3'] = (4.0/3) * math.pi * cs['core_radius_cm']**3
+cs['shell_radius_cm3'] = cs['core_radius_cm'] + cs['shell_size_cm']
+cs['shell_volume_cm3'] = (4.0/3) * math.pi * (cs['shell_radius_cm3'])**3 - cs['core_volume_cm3']
+
+cs['cs_co2_mole'] = cs['a_co2_mole_cm3_core'] * cs['core_volume_cm3']
+# cs['cs_co2_mole'] = cs['a_co2_mole_cm3_shell'] * cs['shell_volume_cm3'] + cs['a_co2_mole_cm3_core'] * cs['core_volume_cm3']
+cs['cs_n2_mole'] = cs['a_n2_mole_cm3_shell'] * cs['shell_volume_cm3'] + cs['a_n2_mole_cm3_core'] * cs['core_volume_cm3']
+cs['cs_h2o_mole'] = cs['h2o_breakthrough_adsorption_mol_cm2'] * 4 * math.pi * cs['core_radius_cm'] ** 2 # this will underestimate for smaller spheres
+cs['cs_co2_fraction'] = cs['cs_co2_mole'] / (cs['cs_co2_mole'] + cs['cs_n2_mole'] + cs['cs_h2o_mole'])
+cs['cs_co2_mole_cm3'] = cs['cs_co2_mole'] / (cs['core_volume_cm3'] + cs['shell_volume_cm3'])
 
 cs['shell_membrane_co2_h2o_selectivity'] = cs['a_co2_mole_cm3_shell'] * cs['d_co2_cm2_s'] / (cs['a_h2o_mole_cm3'] * cs['d_h2o_cm2_s'])
 cs['core_adsorption_co2_n2_selectivity'] = (cs['a_co2_mole_cm3_core'] / 42.18) / (cs['a_n2_mole_cm3_core'] / 79033.50)
-cs['cs_score'] = cs['cs_co2_fraction'] / (400/1000000)
-cs['cs_score_2'] = cs['cs_score'] * cs['cs_co2_mole_cm2']
 
-cs.to_csv("scores-out.csv")
+
+cs['cs_score'] = cs['cs_co2_fraction'] / (400/1000000)
+cs['cs_score_2'] = (cs['cs_score'] - 1) * cs['cs_co2_mole_cm3']
+cs['cs_score_3'] = cs['cs_score_2']  / cs[(cs.mof_core == "UIO-67") & (cs.mof_shell == "UIO-67")]['cs_score_2'].values[0]
+# cs['cs_score_3'] = cs['shell_membrane_co2_h2o_selectivity'] * cs['a_co2_mole_cm3_core']
+# cs['cs_score_4'] = cs['shell_membrane_co2_h2o_selectivity'] * cs['core_adsorption_co2_n2_selectivity']
+# cs['cs_score_3'] = cs['cs_score'] * (1 / ((4/3) * math.pi * (cs['core_size_cm'] + cs['shell_size_cm'])**2)) # this should be mutiplied by pellets per cm3
+
+cs.to_csv("scores-out-spheres-noco2shell-altscores.csv")
 
 
 print(cs)
