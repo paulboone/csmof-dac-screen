e2f3e347b85ad0cc08f28fe74ca0b2809eb17804
Paul Boone <narcissus.pong@gmail.com>
2022-05-12 13:40:05 -0400

    add error bars

--- data-plots/diffusivities-vert.py
+++ data-plots/diffusivities-vert.py
@@ -5,10 +5,9 @@ from matplotlib import rc
 import numpy as np
 import pandas as pd
 
-
+fsl = fs = 8
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-
-fsl = fs = 9
+rc('font', size=fs)
 
 @click.command()
 @click.argument('csv-path', type=click.File())
@@ -18,9 +17,27 @@ def diffusivities(csv_path, outputpath="diffusivities.png"):
     fig = plt.figure(figsize=(6,10))
     data = pd.read_csv(csv_path)
 
-    uio66 = data[data.mof.str.startswith("UIO-66")]
-    uio67 = data[data.mof.str.startswith("UIO-67")]
-    moflabels = list(data.mof.str.replace("UIO-6[67] ", ""))
+    # convert to log10
+    data.d_lower_co2_a2_fs.clip(lower=0, inplace=True)
+    data.d_lower_n2_a2_fs.clip(lower=0, inplace=True)
+    data.d_lower_h2o_a2_fs.clip(lower=0, inplace=True)
+
+    logd_min = -7
+    data['d_co2'] = np.log10(data.d_co2_a2_fs).clip(lower=logd_min + 0.1)
+    data['d_n2'] = np.log10(data.d_n2_a2_fs).clip(lower=logd_min + 0.1)
+    data['d_h2o'] = np.log10(data.d_h2o_a2_fs).clip(lower=logd_min + 0.1)
+
+    data['d_co2_deltl'] = data.d_co2 - np.log10(data.d_lower_co2_a2_fs).clip(lower=logd_min)
+    data['d_co2_deltu'] = np.log10(data.d_upper_co2_a2_fs) - data.d_co2
+    data['d_n2_deltl'] = data.d_n2 - np.log10(data.d_lower_n2_a2_fs).clip(lower=logd_min)
+    data['d_n2_deltu'] = np.log10(data.d_upper_n2_a2_fs) - data.d_n2
+    data['d_h2o_deltl'] = data.d_h2o - np.log10(data.d_lower_h2o_a2_fs).clip(lower=logd_min)
+    data['d_h2o_deltu'] = np.log10(data.d_upper_h2o_a2_fs) - data.d_h2o
+
+    data.mof = data.mof.str.replace("UIO", "UiO")
+    uio66 = data[data.mof.str.startswith("UiO-66")]
+    uio67 = data[data.mof.str.startswith("UiO-67")]
+    moflabels = list(data.mof.str.replace("UiO-6[67] ", ""))
 
     ax = fig.subplots(ncols=1)
 
@@ -42,21 +59,19 @@ def diffusivities(csv_path, outputpath="diffusivities.png"):
     ax.set_ylim(0, len(moflabels))
     ax.set_xlim(-7, -2)
 
-    sc1 = ax.plot(np.log10(uio66.d_co2_a2_fs), uio66y, color="orange", zorder=20, markersize=3, marker="v", label="UIO-66 CO$_2$", linestyle='None')
-    sc1 = ax.plot(np.log10(uio67.d_co2_a2_fs), uio67y, color="orange", zorder=20, markersize=5, marker=r'$\bowtie$', label="UIO-67 CO$_2$", linestyle='None')
-
-    sc1 = ax.plot(np.log10(uio66.d_n2_a2_fs),  uio66y, color="grey", zorder=20, markersize=3, marker="v", label="UIO-66 N$_2$", linestyle='None')
-    sc1 = ax.plot(np.log10(uio67.d_n2_a2_fs),  uio67y, color="grey", zorder=20, markersize=5, marker= r'$\bowtie$', label="UIO-67 N$_2$", linestyle='None')
-
-    sc1 = ax.plot(np.log10(uio66.d_h2o_a2_fs), uio66y, color="blue", zorder=20, markersize=3, marker="v", label="UIO-66 H$_2$O", linestyle='None')
-    sc1 = ax.plot(np.log10(uio67.d_h2o_a2_fs), uio67y, color="blue", zorder=20, markersize=5, marker= r'$\bowtie$', label="UIO-67 H$_2$O", linestyle='None')
+    sc1 = ax.errorbar(uio66.d_co2, uio66y + 0.1, xerr=uio66[["d_co2_deltl", "d_co2_deltu"]].to_numpy().T, color="orange", zorder=20, linestyle='None', lw=1, markersize=3, marker="v",           label="UIO-66 CO$_2$")
+    sc1 = ax.errorbar(uio67.d_co2, uio67y + 0.1, xerr=uio67[["d_co2_deltl", "d_co2_deltu"]].to_numpy().T, color="orange", zorder=20, linestyle='None', lw=1, markersize=5, marker=r'$\bowtie$',  label="UIO-67 CO$_2$")
+    sc1 = ax.errorbar(uio66.d_n2,  uio66y, xerr=uio66[["d_n2_deltl", "d_n2_deltu"]].to_numpy().T, color="grey",           zorder=20, linestyle='None', lw=1, markersize=3, marker="v",           label="UIO-66 N$_2$")
+    sc1 = ax.errorbar(uio67.d_n2,  uio67y, xerr=uio67[["d_n2_deltl", "d_n2_deltu"]].to_numpy().T, color="grey",           zorder=20, linestyle='None', lw=1, markersize=5, marker= r'$\bowtie$', label="UIO-67 N$_2$")
+    sc1 = ax.errorbar(uio66.d_h2o, uio66y - 0.1, xerr=uio66[["d_h2o_deltl", "d_h2o_deltu"]].to_numpy().T, color="blue",   zorder=20, linestyle='None', lw=1, markersize=3, marker="v",           label="UIO-66 H$_2$O")
+    sc1 = ax.errorbar(uio67.d_h2o, uio67y - 0.1, xerr=uio67[["d_h2o_deltl", "d_h2o_deltu"]].to_numpy().T, color="blue",   zorder=20, linestyle='None', lw=1, markersize=5, marker= r'$\bowtie$', label="UIO-67 H$_2$O")
 
     ax.set_yticks(adjusted_break_indices)
     ax.set_yticklabels(["" for _ in adjusted_break_indices])
     ax.set_yticks(range(0,len(moflabels)), minor=True)
-    ax.set_yticklabels(moflabels, fontsize=fs, minor=True)
+    ax.set_yticklabels(moflabels, minor=True, fontsize=fs)
 
-    ax.set_xlabel('log10 diffusivity [Å$^2$ / fs] @ STP', fontsize=fsl)
+    ax.set_xlabel('log10 diffusivity [Å$^2$ / fs] @ STP', fontsize=fs)
 
     ax.grid(which='major', axis='x', linestyle='-', color='0.9', zorder=10)
     ax.grid(which='major', axis='y', linestyle='-', color='0.6', zorder=5)
@@ -65,7 +80,7 @@ def diffusivities(csv_path, outputpath="diffusivities.png"):
     ax.axhline(35, lw=0.75, color='black', zorder=30)
 
     ax.legend()
-    fig.savefig(outputpath, dpi=300, bbox_inches='tight')
+    fig.savefig(outputpath, dpi=600, bbox_inches='tight')
     plt.close(fig)
 
 if __name__ == '__main__':
